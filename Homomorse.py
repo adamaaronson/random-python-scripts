74aef97739b0a5320509cd969b7caa4080d83a02
adamaaronson <adamaaronson@gmail.com>
2019-10-03 08:20:24 -0500

    Added Homomorse

---
+++ Homomorse.py
@@ -0,0 +1,65 @@
+morse = {'a': '.-',
+         'b': '-...',
+         'c': '-.-.',
+         'd': '-..',
+         'e': '.',
+         'f': '..-.',
+         'g': '--.',
+         'h': '....',
+         'i': '..',
+         'j': '.---',
+         'k': '-.-',
+         'l': '.-..',
+         'm': '--',
+         'n': '-.',
+         'o': '---',
+         'p': '.--.',
+         'q': '--.-',
+         'r': '.-.',
+         's': '...',
+         't': '-',
+         'u': '..-',
+         'v': '...-',
+         'w': '.--',
+         'x': '-..-',
+         'y': '-.--',
+         'z': '--..'}
+
+dictionary = open('enable1.txt').read().splitlines()
+
+
+def makemorse(word):
+    morseword = ''
+    word = [l for l in word.lower() if l in 'abcdefghijklmnopqrstuvwxyz'];
+    for l in word:
+        morseword = morseword + morse[l]
+    return morseword
+
+
+def homomorse(words):
+    morsewords = {}
+    for w in words:
+        morsew = makemorse(w)
+        if morsew in morsewords.keys():
+            morsewords[morsew].append(w)
+        else:
+            morsewords[morsew] = [w]
+
+    homomorse = {key: value for (key, value) in morsewords.items() if len(value) > 1}
+    return homomorse
+
+
+def commas(word):
+    return word.count(',')
+
+
+def mits(word):
+    return word.index(' ')
+
+
+words = dictionary
+homomorse = homomorse(words)
+
+hmstrings = [m + '   ' + ', '.join(homomorse[m]) for m in homomorse.keys()]
+
+print('\n'.join(sorted(hmstrings, key=mits, reverse=True)))
